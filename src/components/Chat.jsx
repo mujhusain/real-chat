5501a7e465f7e2dda85168b14674ab8774a02c23
Mujaffar Husain <93472360+mujhusain@users.noreply.github.com>
2023-08-06 00:34:35 +0530

    chat container styling compleated

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,19 @@
 import React from "react"
+import cam from "../img/cam.png";
+import add_user from "../img/add_user.png";
+import more from "../img/more.png";
 
 const Chat = (props) => {
   return (
     <div className="chat">
-        Chat
+        <div className="chatInfo">
+            <span>Muajffar</span>
+            <div className="otherFeaters">
+                <img src={cam} alt="Camera" />
+                <img src={add_user} alt="Add" />   
+                <img src={more} alt="more" />
+            </div>
+        </div>
     </div>
   )
 };
