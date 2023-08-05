3fb0568af9443b7860e3d80dd7c816c0aeb13358
Mujaffar Husain <93472360+mujhusain@users.noreply.github.com>
2023-08-06 01:29:20 +0530

    input style completed

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import cam from "../img/cam.png";
 import add_user from "../img/add_user.png";
 import more from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat = (props) => {
   return (
@@ -14,6 +16,8 @@ const Chat = (props) => {
                 <img src={more} alt="more" />
             </div>
         </div>
+        <Messages/>
+        <Input/>
     </div>
   )
 };
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React from "react";
+import file from "../img/file.png";
 
 const Input = (props) => {
   return (
-    <div>
-      Input
+    <div className="inputContainer">
+      <input type="text" placeholder="Message..." />
+      <div className="send">
+        <input
+          type="file"
+          style={{ display: "none" }}
+          name=""
+          id="attachFile"
+        />
+        <label htmlFor="attachFile">
+          <img src={file} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Input;
