cc65925f8b1efe12f0605d4655f4661189a90a82
Mujaffar Husain <93472360+mujhusain@users.noreply.github.com>
2023-08-09 00:48:45 +0530

    dynamic navbar data implimenting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import user from "../img/user.png";
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContextProvider";
 
 const Navbar = (props) => {
+  const {currentUser}=useContext(AuthContext);
   return (
     <div className="navbar">
         <span className="logo" >RealChat 💬</span>
         <div className="user">
-            <img src={user} alt="" />
-            <span className="userName">Mujaffar</span>
+            <img src={currentUser.photoURL} alt="" />
+            <span className="userName">{currentUser.displayName}</span>
             <button onClick={()=>signOut(auth)} >Logout</button>
         </div>
     </div>
