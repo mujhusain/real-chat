701d2a7852d212e7e024b929705d17456b61d23a
Mujaffar Husain <93472360+mujhusain@users.noreply.github.com>
2023-08-11 00:42:36 +0530

    usersChats list listed in sidebar

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,30 +1,39 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContextProvider";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
 const Chats = (props) => {
-  const [chats,setChats]=useState([]);
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+      return () => {
+        unsub();
+      };
+    };
+    
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+  console.log("Current data: ", Object.entries(chats));
 
   return (
     <div className="chats">
-      <div className="userChat">
+      {Object.entries(chats)?.map((chat)=>
+      <div className="userChat" key={chat[0]}  >
         <img
-          src="https://th.bing.com/th/id/OIP.F2XOBhKXhiDs8Fv-Tde6YgHaIJ?w=196&h=216&c=7&r=0&o=5&dpr=2&pid=1.7"
-          alt=""
+          src={chat[1].userInfo.photoURL}
+          alt={chat[1].userInfo.displayName}
         />
         <div className="userChatInfo">
-          <span>Kim Min</span>
-          <p>hello Jane</p>
+          <span>{chat[1].userInfo.displayName}</span>
+          <p>{chat[1].userInfo?.lastMessage?.text}</p>
         </div>
       </div>
-      <div className="userChat">
-        <img
-          src="https://th.bing.com/th/id/OIP.F2XOBhKXhiDs8Fv-Tde6YgHaIJ?w=196&h=216&c=7&r=0&o=5&dpr=2&pid=1.7"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Kim Min</span>
-          <p>hello Jane</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
